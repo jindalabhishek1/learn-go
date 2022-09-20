e71f6614a3b52ee9cd97018e8b4aa74a465aca0d
Jindal, Abhishek <abhishekjindal.jindal74@gmail.com>
2022-09-20 22:30:41 +0530

    Day 02: Returning error from greetings.go in case of empty name

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,14 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
 
 // Hello returns s greeting for the named person.
 // Syntax: func <name of the function> (<parameter name> <parameter type>) <return type> {
-func Hello(name string) string {
+func Hello(name string) (string, error) {
     // Returns a greeting that embeds the name in a message.
+
+    // If no name was given, return an error with a message.
+    if name == "" {
+        retun "", errors.New("empty name")
+    }
+
+    // If a name was received, return a value that embeds the name in a greeting message.
+
     // Syntax: <variable name> := <something>
     // := is shortcut for declaring and initializing a variable
     // Same as: var message string
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
